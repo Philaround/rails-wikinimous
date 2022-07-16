4de9eebce78d1929ca6049f2fe7cf95dad92e421
Filippos Lavrentidis <n.p.lavrentidis@gmail.com>
2022-07-16 16:39:02 +0300

    initial coding and faker articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Article.create(
+    title: Faker::String.random(length: 4),
+    content: Faker::Lorem.sentence
+  )
+end
+g
